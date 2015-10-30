10c7fea99311920883d5370f8522e538981bb7c4
beezy <brian.stumbaugh@gmail.com>
2015-10-30 07:22:43 -0500

    click it

--- javascript-understandingTheWeirdParts/app.js
+++ javascript-understandingTheWeirdParts/app.js
@@ -1,9 +1,23 @@
-var a;
-console.log(a);
+// long running function
+function waitThreeSeconds() {
+    var ms = 3000 + new Date().getTime();
+    while (new Date() < ms){}
+    console.log('finished function');
+}
 
-if (a === undefined) {
-	console.log("a is undefined!");
+function clickHandler() {
+    console.log('click event!');   
 }
-else {
-	console.log("a is defined");
-}
+
+// listen for the click event
+document.addEventListener('click', clickHandler);
+
+
+waitThreeSeconds();
+console.log('finished execution');
+
+
+// BE SURE TO CLICK IN BROWSER WHEN WAITING THE THREE SECOND PAGE TO LOAD
+//the click console.logs the click last......it's explained in the video. 
+// the page does everything in it's stack....and then looks to the EVENT QUEUE to see 
+ // if anything happened....a click happenend...so it tells us that after
