8d3b694f17c1308725f43211950aaadcac03cb74
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2015-10-29 22:48:52 -0500

    im tired

--- javascript-understandingTheWeirdParts/exe.js
+++ javascript-understandingTheWeirdParts/exe.js
@@ -1,13 +1,23 @@
 function b() {
 	var myVar;
+	console.log(myVar);
 }
 
 function a() {
 	var myVar = 2;
+	console.log(myVar);     
 	b();
 }
 
 var myVar = 1;
-a();
+console.log(myVar);
 
-var x = 3;
+a();
+console.log(myVar);   //myVar will be 1 again here....because it is unchanged after everything fires
+
+
+//this whole program displays:
+// 1
+// 2
+// undefined
+// 1
