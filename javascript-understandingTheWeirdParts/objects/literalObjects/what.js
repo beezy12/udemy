4c7e34793642924474c431e21d0055f01f2264b3
beezy <brian.stumbaugh@gmail.com>
2015-10-30 23:45:17 -0500

    yah

---
+++ javascript-understandingTheWeirdParts/objects/literalObjects/what.js
@@ -0,0 +1,5 @@
+// instead of doing     var person = new Object();      
+// a literal object is   var person = {};
+
+var person = { firstName: "Brian", lastName: "Stumbaugh" };
+console.log(person);
